d3ef736fb08ba13ee0ff99530b093437987139ea
agaro1121 <agaro1121@gmail.com>
2025-05-12 19:35:32 -0400

    deletes default telescope mappings

--- lua/cheatsheet/config.lua
+++ lua/cheatsheet/config.lua
@@ -22,12 +22,7 @@ local defaults = {
     -- same directory name)
     include_only_installed_plugins = true,
     -- Key mappings bound inside the telescope window
-    telescope_mappings = {
-        ['<CR>'] = require('cheatsheet.telescope.actions').select_or_fill_commandline,
-        ['<A-CR>'] = require('cheatsheet.telescope.actions').select_or_execute,
-        ['<C-Y>'] = require('cheatsheet.telescope.actions').copy_cheat_value,
-        ['<C-E>'] = require('cheatsheet.telescope.actions').edit_user_cheatsheet,
-    }
+    telescope_mappings = {}
 }
 
 M.options = {}
